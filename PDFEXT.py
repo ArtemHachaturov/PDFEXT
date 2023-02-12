de3dd57be2f28778df8d2e2e63bac5fe90e4ee9c
ArtemHachaturov <109848233+ArtemHachaturov@users.noreply.github.com>
2023-02-12 11:08:45 +0500

    Update PDFEXT.py

--- PDFEXT.py
+++ PDFEXT.py
@@ -3,15 +3,14 @@ from PyPDF2 import PdfReader, PdfWriter
 from tqdm import trange
 from time import sleep
 
-#Ввод данных 
+#Data input
 print('\n***WELCOME TO PDFEXT SUPERACTION PROGRAM V1.0.0 BETA VERSION***')
 
 print('\n\033[31m   Before performing certain manipulations with a PDF document, do the following:\n1. Make sure that the document consists exclusively of files that should be disassembled by name, remove the rest of the files from the PDF.\n2.Check the value of the name in the files, for the absence of instances and the same font size.\033[0m')
 
-
 enter =  input('\nIf you read successfully, press ENTER...')
 
-#Считываем файл 
+#Read the file
 while True:
     try:
         inpNameFile = input('\n\033[32m>>>Enter the name of the document to form into separate sheets...for example:\033[31m File for testing...\033[32m\033[0m')
@@ -23,12 +22,10 @@ while True:
         
 inpNamelist = input('\n\033[32m>>>Enter the name of the file indicated on the first page...for example: \033[31m 1AI1...\033[0m ')
 
-
-
-#Изымаем кол страниц 
+#Remove the number of pages
 numPages = len(reader.pages)
 
-#Создаем пустые списки 
+#Create empty lists 
 parts0 = []
 parts1 = []
 
@@ -36,8 +33,7 @@ print('\n Just a moment, data is being retrieved to determine the file name\n')
 for i in trange(numPages):
     #Обращаемся к первой страницы файла 
     page = reader.pages[i]
-
-    #Помещаем в списки текст страницы и высоты шрифта 
+    #Put page text and font height into lists 
     def visitor_body(text, cm, tm, fontDict, fontSize):
 
         parts0.append(text) 
@@ -45,21 +41,17 @@ for i in trange(numPages):
                 
     page.extract_text(visitor_text=visitor_body)
 
-#Извлекаем индекс текста с списка  
+#Retrieve the text index from the list 
 indexNum = parts0.index(str(inpNamelist))
 
-#Находим по индексу высоту шрифта 
+#Find the font height by index
 inpFontSize =  parts1[indexNum]
 
 print('\n Assignment and unpacking has already begun\n')
 for b in trange(numPages):
-
-    
-        #Создаем пустой список для каждой страницы 
-        parts = []
-        #Помещаем лист 
+        parts = [] 
         page = reader.pages[b]
-        #Помещаем найденый по индексу высоту текста и находим ее в данном листе по #средством сравнеия, после чего изымем данный текст и применяем его в качестве #названия файла.
+        #Place the height of the text found by the index and find it in this sheet using the comparison tool, after which we remove this text and use it as the file name.
         def visitor_body1(text, cm, tm, fontDict, fontSize):
         
             if fontSize == float(inpFontSize):
@@ -68,7 +60,7 @@ for b in trange(numPages):
         page.extract_text(visitor_text=visitor_body1)
         text_body = "".join(parts)
 
-        #Сохраняем файл с определенным именем каждого листа 
+        #Save the file with a specific name for each sheet
         pdfWriter = PdfWriter()
         pdfWriter.add_page(page)
         fileName = str(text_body) + '.pdf'
